cc70bf130a481bc6f5b2e4c6a4711cc4590a6970
Thiago-Krieger <trkrgr@gmail.com>
2022-01-16 16:19:40 -0300

    EstruturaBasica

---
+++ Controllers/DespesasController.cs
@@ -0,0 +1,154 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Financas.Dominio;
+using Financas.Repositorio;
+
+namespace DesafioPubFuture.Controllers
+{
+    public class DespesasController : Controller
+    {
+        private readonly FinancasContext _context;
+
+        public DespesasController(FinancasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Despesas
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Despesas.ToListAsync());
+        }
+
+        // GET: Despesas/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var despesa = await _context.Despesas
+                .FirstOrDefaultAsync(m => m.DespesaId == id);
+            if (despesa == null)
+            {
+                return NotFound();
+            }
+
+            return View(despesa);
+        }
+
+        // GET: Despesas/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Despesas/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DespesaId,Valor,DataPagamento,DataPagamentoEsperado,TipoDespesa,ContaId")] Despesa despesa)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(despesa);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(despesa);
+        }
+
+        // GET: Despesas/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var despesa = await _context.Despesas.FindAsync(id);
+            if (despesa == null)
+            {
+                return NotFound();
+            }
+            return View(despesa);
+        }
+
+        // POST: Despesas/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DespesaId,Valor,DataPagamento,DataPagamentoEsperado,TipoDespesa,ContaId")] Despesa despesa)
+        {
+            if (id != despesa.DespesaId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(despesa);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DespesaExists(despesa.DespesaId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(despesa);
+        }
+
+        // GET: Despesas/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var despesa = await _context.Despesas
+                .FirstOrDefaultAsync(m => m.DespesaId == id);
+            if (despesa == null)
+            {
+                return NotFound();
+            }
+
+            return View(despesa);
+        }
+
+        // POST: Despesas/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var despesa = await _context.Despesas.FindAsync(id);
+            _context.Despesas.Remove(despesa);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DespesaExists(int id)
+        {
+            return _context.Despesas.Any(e => e.DespesaId == id);
+        }
+    }
+}
